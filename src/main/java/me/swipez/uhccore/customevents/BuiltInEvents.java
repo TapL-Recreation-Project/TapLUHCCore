a22d813637d2cf7cf79a9e3a40d9e4f3c2208b3d
yehudahrrs <47502993+yehudahrrs@users.noreply.github.com>
2021-03-04 08:52:05 -0500

    Added cutclean

--- src/main/java/me/swipez/uhccore/customevents/BuiltInEvents.java
+++ src/main/java/me/swipez/uhccore/customevents/BuiltInEvents.java
@@ -9,6 +9,7 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Locale;
@@ -23,13 +24,21 @@ public class BuiltInEvents implements Listener {
 
     @EventHandler
     public void onBreak(BlockBreakEvent e) {
-        Bukkit.broadcastMessage("block broked");
         if (TIMBER && UHCAPI.isStarted && (e.getBlock().getType().toString().toLowerCase().contains("_log"))) {
             Material heldMaterial = e.getPlayer().getInventory().getItemInMainHand().getType();
             Material brokenMaterial = e.getBlock().getType();
             if (heldMaterial.toString().toLowerCase().contains("_axe")
                     && brokenMaterial.toString().toLowerCase().contains("_log")) {
                 breakAdjacentWood(e.getBlock().getLocation(), e.getBlock().getType());
+            }
+        }
+        if (CUT_CLEAN && UHCAPI.isStarted && e.getBlock().getType().toString().toLowerCase().contains("_ore") && e.getPlayer().getInventory().getItemInMainHand().getType().toString().toLowerCase().contains("_pickaxe") && !e.getPlayer().getInventory().getItemInMainHand().getType().toString().toLowerCase().contains("wooden_")) {
+            if (e.getBlock().getType().equals(Material.IRON_ORE)) {
+                e.setDropItems(false);
+                e.getBlock().getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT));
+            } else if (e.getBlock().getType().equals(Material.GOLD_ORE) && !e.getPlayer().getInventory().getItemInMainHand().getType().toString().toLowerCase().contains("stone_")) {
+                e.setDropItems(false);
+                e.getBlock().getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(), new ItemStack(Material.GOLD_INGOT));
             }
         }
     }
