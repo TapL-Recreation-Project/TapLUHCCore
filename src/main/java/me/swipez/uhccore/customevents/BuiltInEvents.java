b98a899ae5146a9eb4661406610beb4b0614247f
Caio P <caiopq98@gmail.com>
2021-03-03 21:21:05 -0300

    Implement instant wood breaking

--- src/main/java/me/swipez/uhccore/customevents/BuiltInEvents.java
+++ src/main/java/me/swipez/uhccore/customevents/BuiltInEvents.java
@@ -2,6 +2,7 @@ package me.swipez.uhccore.customevents;
 
 import me.swipez.uhccore.api.UHCAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -23,24 +24,25 @@ public class BuiltInEvents implements Listener {
     @EventHandler
     public void onBreak(BlockBreakEvent e) {
         Bukkit.broadcastMessage("block broked");
-        if (TIMBER && UHCAPI.isStarted && (e.getBlock().getType().toString().toLowerCase().contains("log"))) {
-            Bukkit.broadcastMessage("log broked");
-
-
-            Block checkblock = e.getBlock();
-            for (int i=e.getBlock().getY();i<256-e.getBlock().getY();i++){
-                Bukkit.broadcastMessage("in for loop " + i);
-
-                if (checkblock.getType().toString().toLowerCase().contains("log")){
-                    Bukkit.broadcastMessage("in if " + i);
-
-                    checkblock.breakNaturally();
-                    checkblock = checkblock.getRelative(BlockFace.UP);
-                } else {
-                    return;
-                }
+        if (TIMBER && UHCAPI.isStarted && (e.getBlock().getType().toString().toLowerCase().contains("_log"))) {
+            Material heldMaterial = e.getPlayer().getInventory().getItemInMainHand().getType();
+            Material brokenMaterial = e.getBlock().getType();
+            if (heldMaterial.toString().toLowerCase().contains("_axe")
+                    && brokenMaterial.toString().toLowerCase().contains("_log")) {
+                breakAdjacentWood(e.getBlock().getLocation(), e.getBlock().getType());
             }
+        }
+    }
 
+    public void breakAdjacentWood(Location origin, Material woodMaterial) {
+        BlockFace[] blockFaces = new BlockFace[] { BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST,
+                BlockFace.UP, BlockFace.DOWN };
+        for (BlockFace face : blockFaces) {
+            Block relative = origin.getBlock().getRelative(face);
+            origin.getBlock().breakNaturally();
+            if (relative.getType() == woodMaterial) {
+                breakAdjacentWood(relative.getLocation(), woodMaterial);
+            }
         }
     }
 }
